ca2ed69c6631281935f3dc810ddfbeae5572e02e
Shahnawaz <shahnawazkhan333@live.com>
2024-02-11 22:48:41 +0500

    Added my project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+	BrowserRouter as Router,
+	Routes, Route,
+	Link, useNavigate
+} from 'react-router-dom';
+import Appointments
+	from './components/Appointments';
+import Doctors from './components/Doctors';
+import Patients from './components/Patients';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	const isLinkActive =
+		(path) =>
+			window.location.pathname === path;
+	return (
+		<Router>
+			<div className="container">
+				<h1 style={{ color: "brown" }}>
+					 Hospital Managment App
+				</h1>
+				<nav>
+					<ul>
+						<li className={
+							isLinkActive('/appointments')
+								? 'active' : ''}>
+							<Link to="/appointments">
+								Appointments
+							</Link>
+						</li>
+						<li className={
+							isLinkActive('/doctors') ?
+								'active' : ''}>
+							<Link to="/doctors">
+								Doctors
+							</Link>
+						</li>
+						<li className={
+							isLinkActive('/patients') ?
+								'active' : ''}>
+							<Link to="/patients">
+								Patients
+							</Link>
+						</li>
+					</ul>
+				</nav>
+
+				<Routes>
+					<Route path="/appointments"
+						element={<Appointments />} />
+					<Route path="/"
+						element={<Appointments />} />
+					<Route path="/doctors"
+						element={<Doctors />} />
+					<Route path="/patients"
+						element={<Patients />} />
+				</Routes>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
